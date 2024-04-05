d2e16e4104812efd778e6005fc836def360ba321
Stephen Kalogridis <stephen.kalogridis@gmail.com>
2024-04-05 14:07:21 -0400

    Adjust webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -40,7 +40,7 @@ module.exports = {
   ],
   devServer: {
     static: {
-      directory: path.join(__dirname, 'dist'),
+      directory: path.join(__dirname, 'docs'),
     },
     compress: true,
     port: 9000,
