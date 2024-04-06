353f4e52c93728fbefb3d300ff2c726a55b3248b
Stephen Kalogridis <stephen.kalogridis@gmail.com>
2024-04-06 13:49:55 -0400

    Adjust found words functinoality and add theme toggle control

--- README.md
+++ README.md
@@ -12,5 +12,9 @@ Built using vanilla JavaScript and a Wordlist
   - Should be able to click on to view more detailed score breakdown
 - Implement Previous Word List
   - Should give preview of most recent accepted words
+  - Overflow should be hidden, perhaps with a fading effect
   - Should have functinoality to show all previous words
   - Should offer to pull definition from Webster Dictionary API
+- Getting Spangram or Septagram should award addiontal points
+- Center letter should be orange in input queue
+- New Game should clear found word list and array
--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const words = require('./wordsArray.js');
 
 // GLOBALS
 const newGameBtn = document.querySelector('.new-game');
+const themeToggle = document.querySelector('#theme-toggle');
 const inputQueue = document.querySelector('#input-queue');
 const deleteBtn = document.querySelector('#delete');
 const shuffleBtn = document.querySelector('#shuffle');
@@ -48,10 +49,14 @@ let centerLetter;
 
 // WORD ARRAY
 let wordsArray = [];
-const foundArray = [];
+let foundArray = [];
 
 function updateFoundWords() {
   foundWords.textContent = foundArray.join(', ');
+}
+
+function toCapitalCase(word) {
+  return word.slice(0, 1).toUpperCase() + word.slice(1).toLowerCase();
 }
 
 // HEX ARRAY
@@ -104,7 +109,7 @@ function assignLetters() {
     return array.filter((item) => item.vowel === true).length >= 2;
   }
   function chooseLetters() {
-    const newList = [...alphabetArray]; // Make a copy of alphabetArray
+    const newList = [...alphabetArray];
     for (let hex of hexes) {
       const index = getRandomIntInclusive(0, newList.length - 1);
       const letter = newList[index];
@@ -193,13 +198,12 @@ function buildGame(num) {
   if (wordsArray.length < num) {
     clearGame();
     buildGame(num);
-  } else {
-    activateHexes();
   }
 }
 
 // On Load Fucntions
 window.onload = () => {
+  activateHexes();
   buildGame(80);
   console.log(wordsArray);
 };
@@ -241,11 +245,11 @@ shuffleBtn.addEventListener('click', () => {
 submitBtn.addEventListener('click', () => {
   const input = inputQueue.textContent;
   if (wordsArray.includes(input) && !foundArray.includes(input)) {
-    // if input has all of letters in lettersArray
-    if ('crazy thing') {
-      console.log('Spangram');
-      printMessage('Spangram!');
-    }
+    // // if input has all of letters in lettersArray
+    // if ('crazy thing') {
+    //   console.log('Septagram');
+    //   printMessage('Septagram!');
+    // }
     console.log('Correct!');
     printMessage('Correct!');
     if (input.length === 4) {
@@ -253,7 +257,10 @@ submitBtn.addEventListener('click', () => {
     } else {
       score += input.length;
     }
-    foundArray.push(input);
+    console.log('Before', foundArray);
+    foundArray.unshift(toCapitalCase(input));
+    console.log('After', foundArray);
+
     updateFoundWords();
     updateScore();
   } else if (foundArray.includes(input)) {
@@ -272,4 +279,20 @@ submitBtn.addEventListener('click', () => {
     }
   }
   inputQueue.textContent = '';
+});
+
+themeToggle.addEventListener('click', () => {
+  const orange = '#fea635';
+  const light = '#fffbff';
+  const dark = '#554f3a';
+  const root = document.querySelector(':root');
+  const rootStyles = getComputedStyle(root);
+
+  if (rootStyles.getPropertyValue('--background') === light) {
+    root.style.setProperty('--background', dark);
+    root.style.setProperty('--accent', light);
+  } else {
+    root.style.setProperty('--background', light);
+    root.style.setProperty('--accent', dark);
+  }
 });
